a1ac1d3cf88228167d42b6cf8018758b4d97d2b0
AXEL SAJNOVSKY <41709606@ort>
2016-09-20 13:34:06 -0300

    Initial commit

---
+++ app/src/main/java/com/example/a41709606/juegololazo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a41709606.juegololazo;
+
+         import android.support.v7.app.AppCompatActivity;
+         import android.os.Bundle;
+         import android.view.WindowManager;
+         import org.cocos2d.opengl.CCGLSurfaceView;
+
+
+        public class MainActivity extends AppCompatActivity {
+
+
+            CCGLSurfaceView VistaPrincipal;
+
+
+                @Override
+       protected void onCreate(Bundle savedInstanceState) {
+               super.onCreate(savedInstanceState);
+                 getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                VistaPrincipal = new CCGLSurfaceView(this);
+               setContentView(VistaPrincipal);
+           }
+      @Override
+         protected void onStart() {
+             super.onStart();
+               clsJuego mijuego;
+              mijuego= new clsJuego(VistaPrincipal);
+               mijuego.ComenzarJuego();
+      }
+        }
---
+++ app/src/main/java/com/example/a41709606/juegololazo/clsJuego.java
@@ -0,0 +1,202 @@
+package com.example.a41709606.juegololazo;
+
+/**
+ * Created by 41709606 on 20/9/2016.
+ */
+
+        import android.util.Log;
+
+        import org.cocos2d.actions.interval.MoveTo;
+        import org.cocos2d.actions.interval.RotateTo;
+        import org.cocos2d.actions.interval.ScaleBy;
+        import org.cocos2d.layers.Layer;
+        import org.cocos2d.nodes.CocosNode;
+        import org.cocos2d.nodes.Director;
+        import org.cocos2d.nodes.Label;
+        import org.cocos2d.nodes.Scene;
+        import org.cocos2d.nodes.Sprite;
+        import org.cocos2d.opengl.CCGLSurfaceView;
+        import org.cocos2d.types.CCPoint;
+        import org.cocos2d.types.CCSize;
+
+        import java.util.Random;
+        import java.util.Timer;
+        import java.util.TimerTask;
+
+
+public class clsJuego {
+    CCGLSurfaceView _VistaDelJuego;
+    CCSize PantallaDelDispositivo;
+    Sprite NaveJugador, ImagenFondo, NaveEnemiga;
+    Label lblTituloJuego;
+
+
+    public clsJuego(CCGLSurfaceView VistaDelJuego) {
+        Log.d("Bob", "Comienza el constructor de la clase");
+        _VistaDelJuego = VistaDelJuego;
+    }
+
+    public void ComenzarJuego() {
+        Log.d("Comenzar", "Comienza el juego");
+        Director.sharedDirector().attachInView(_VistaDelJuego);
+
+
+        PantallaDelDispositivo = Director.sharedDirector().displaySize();
+        Log.d("Comenzar", "Pantalla del dispositivo - ancho:" + PantallaDelDispositivo.width + " - alto: " + PantallaDelDispositivo.height);
+        Director.sharedDirector().runWithScene(EscenaDelJuego());
+
+
+    }
+
+    private Scene EscenaDelJuego() {
+        Log.d("EscenaDelJuego", "Comienza el armado de escena");
+
+
+        Log.d("EscenaDelJuego", "Declaro e instancio la escena del juego en si");
+        Scene EscenaADevolver;
+        EscenaADevolver = Scene.node();
+
+        Log.d("EscenaDelJuego", "Declaro e instancio la capa que va a contener la imagen de fondo");
+        CapaDeFondo MiCapaFondo = new CapaDeFondo();
+        CapaFrente MiCapaFrente = new CapaFrente();
+
+
+        Log.d("EscenaDelJuego", "Declaro e instancio la capa que va a contener el jugador y a los enemigos");
+        EscenaADevolver.addChild(MiCapaFondo, -10);
+        EscenaADevolver.addChild(MiCapaFrente, 10);
+
+
+        Log.d("EscenaDelJuego", "Devuelvo la escena ya armada");
+        return EscenaADevolver;
+    }
+
+
+    class CapaDeFondo extends Layer {
+        public CapaDeFondo() {
+            Log.d("CapaDelFondo", "Comienza el constructor de la capa del fondo");
+
+            Log.d("CapaDelFondo", "Pongo la imagen del fondo del juego");
+            PonerImagenFondo();
+        }
+
+        private void PonerImagenFondo() {
+            Log.d("PonerImagenFondo", "Comienzo a poner la imagen del fondo");
+
+            Log.d("PonerImagenFondo", "Instancio el sprite");
+            ImagenFondo = Sprite.sprite("fondo.png");
+
+            Log.d("PonerImagenFondo", "La ubico en el centro de la pantalla");
+            ImagenFondo.setPosition(PantallaDelDispositivo.width / 2, PantallaDelDispositivo.height / 2);
+
+            Log.d("PonerImagenFondo", "Agrando la imagen al doble de su tamaño actual");
+            ImagenFondo.runAction(ScaleBy.action(0.01f, 2.0f, 2.0f));
+
+            Log.d("PonerImagenFondo", "Lo agrego a la capa");
+            super.addChild(ImagenFondo);
+        }
+    }
+
+
+    private class CapaFrente extends Layer {
+        public CapaFrente() {
+            Log.d("CapaDelFrente", "Comienza el constructor de la capa del frente");
+
+            Log.d("CapaDelFrente", "Pongo el jugador en su posicion inicial");
+            PonerNaveJugadorPosicionInicial();
+            PonerTitulo();
+
+            TimerTask TareaPonerEnemigos;
+            TareaPonerEnemigos = new TimerTask() {
+                @Override
+                public void run() {
+                    PonerUnEnemigo();
+                }
+            };
+            Timer RelojEnemigos;
+            RelojEnemigos = new Timer();
+            RelojEnemigos.schedule(TareaPonerEnemigos, 0, 1000);
+        }
+
+
+
+    private void PonerNaveJugadorPosicionInicial() {
+        Log.d("PonerNaveJugador", "Comienzo a poner la nave del jugador en su posicion inicial");
+        Log.d("PonerNaveJugador", "Instancio el sprite");
+        NaveJugador = Sprite.sprite("rocket_mini.png");
+
+        float PosicionInicialX, PosicionInicialY;
+        Log.d("PonerNaveJugador", "Obtengo la mitad del ancho de la pantalla");
+        PosicionInicialX = PantallaDelDispositivo.width / 2;
+
+        Log.d("PonerNaveJugador", "Obtengo la mitad de la alatura de la nave");
+        PosicionInicialY = NaveJugador.getHeight() / 2;
+
+        Log.d("PonerNaveJugador", "Lo ubico en X: " + PosicionInicialX + " -Y:" + PosicionInicialY);
+        NaveJugador.setPosition(PosicionInicialX, PosicionInicialY);
+
+        Log.d("PonerNaveJugador", "Lo agrego a la capa");
+        super.addChild(NaveJugador);
+    }
+
+    private void PonerTitulo() {
+
+        Log.d("PonerTitulo", "Comienzo a poner el titulo");
+        lblTituloJuego=Label.label("Super mega juegazo", "Verdana", 30);
+
+        float AltoDelTitulo;
+        AltoDelTitulo=lblTituloJuego.getHeight();
+
+        lblTituloJuego.setPosition(PantallaDelDispositivo.width/2, PantallaDelDispositivo.height-AltoDelTitulo/2);
+        super.addChild(lblTituloJuego);
+    }
+    void PonerUnEnemigo() {
+        Log.d("PonerEnemigo", "Instancio el sprite del enemigo");
+        NaveEnemiga = Sprite.sprite("enemigo.gif");
+
+        Log.d("PonerEnemigo", "Determino la posicion inicial");
+        CCPoint PosicionInicial;
+        PosicionInicial = new CCPoint();
+
+        Log.d("PonerEnemigo", "La posicion Y es arriba de todo de la pantalla, totalmente afuera");
+        float AlturaEnemigo, AnchoEnemigo;
+        AlturaEnemigo = NaveEnemiga.getHeight();
+        AnchoEnemigo = NaveEnemiga.getWidth();
+        PosicionInicial.y=PantallaDelDispositivo.height + AlturaEnemigo/2;
+
+        Log.d("PonerEnemigo", "Declaro e incializo el generador de azar");
+        Random GeneradorDeAzar;
+        GeneradorDeAzar=new Random();
+
+        Log.d("PonerEnemigo", "La posicion Y es arriba de todo de la pantalla");
+        PosicionInicial.y = PantallaDelDispositivo.height;
+
+        Log.d("PonerEnemigo", "La posicion X es en el centro");
+        PosicionInicial.x = GeneradorDeAzar.nextInt((int) PantallaDelDispositivo.width-(int)AnchoEnemigo)+ AnchoEnemigo/2;
+
+        Log.d("PonerEnemigo", "La ubico en las posiciones que determiné");
+        NaveEnemiga.setPosition(PosicionInicial.x, PosicionInicial.y);
+
+        Log.d("PonerEnemigo", "Lo roto para que mire hacia abajo");
+        NaveEnemiga.runAction(RotateTo.action(0.01f, 180f));
+
+        Log.d("PonerEnemigo", "Determino la posicion final");
+        CCPoint PosicionFinal;
+        PosicionFinal = new CCPoint();
+
+        Log.d("PonerEnemigo", "La posicion final X va a ser igual que la inicial");
+        PosicionFinal.x=PosicionInicial.x;
+
+        Log.d("PonerEnemigo", "La posicion final Y va a ser abajo de todo");
+        PosicionFinal.y=0;
+
+        Log.d("PonerEnemigo", "La posicion final Y va a ser abajo de todo");
+        PosicionFinal.y= -AlturaEnemigo/2;
+
+        Log.d("PonerEnemigo","Le digo que se desplace hacia la posicion final, y demore 3 segundos en hacerlo");
+        NaveEnemiga.runAction(MoveTo.action(3, PosicionFinal.x, PosicionFinal.y));
+
+        Log.d("PonerEnemigo", "Agrego el sprite a la capa");
+        super.addChild(NaveEnemiga);
+    }
+    }
+}
